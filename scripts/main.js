464acac42422a15a5b0cf56fb56123e9659bd3f1
Hey-nes <105350101+Hey-nes@users.noreply.github.com>
2023-10-16 18:56:22 +0200

    finished styling, added js to fetch product dummy data

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,29 @@
-document.querySelector(".cart-toggle").addEventListener("click", () => {
+document.querySelector(".cart-open").addEventListener("click", () => {
   document.querySelector(".app").classList.toggle("cart-open");
 });
+
+document.querySelector(".cart-close").addEventListener("click", () => {
+  document.querySelector(".app").classList.remove("cart-open");
+});
+
+fetch("https://fakestoreapi.com/products")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    var productCards = document.querySelectorAll(".product-card");
+
+    for (var i = 0; i < productCards.length; i++) {
+      var productData = data[i];
+
+      productCards[i].querySelector("h3").textContent = productData.title;
+      productCards[i]
+        .querySelector("img")
+        .setAttribute("src", productData.image);
+      productCards[i].querySelector("p").textContent = productData.description;
+      productCards[i].querySelector("strong").textContent =
+        productData.price + "$";
+      productCards[i].querySelector("span").textContent =
+        productData.rating.rate + "/10";
+    }
+  });
